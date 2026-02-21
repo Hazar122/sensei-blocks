5325d64df7471fb76a1d5e987a59222a595b296a
Hazar122 <67770254+Hazar122@users.noreply.github.com>
2026-02-21 12:59:29 +0000

    Update main.blocks, senseisblocks.ts

--- senseisblocks.ts
+++ senseisblocks.ts
@@ -65,6 +65,9 @@ namespace senseisBlocks {
     }
 
 
+
+
+
     const INTERNAL_POINTER_DOWN = 6868;
     type MouseHandler = (x: number, y: number) => void;
     let _handler: MouseHandler;
@@ -83,6 +86,150 @@ namespace senseisBlocks {
     //% weight=100
     export function onLeftClick(handler: (x: number, y: number) => void) {
         _handler = handler;
+    }
+
+    //% block="is x $x y $y between x1 $xpos1 y1 $ypos1 x2 $xpos2 y2 $ypos2"
+    //% subcategory="Mouse"
+    //% inlineInputMode=inline
+    //% weight=99
+    export function isBetweenCoords(
+        x: number, y: number,
+        xpos1: number, xpos2: number,
+        ypos1: number, ypos2: number
+    ): boolean {
+        return (x >= xpos1 && x <= xpos2 && y >= ypos1 && y <= ypos2)
+    }
+
+
+
+
+
+
+    /**
+     * Convert a string to upper case
+     */
+    //% block="string $str to upper case"
+    //% subcategory="String"
+    //% weight=90
+    export function stringToUpper(str: string): string {
+        return str.toUpperCase();
+    }
+
+    /**
+     * Convert a string to lower case
+     */
+    //% block="string $str to lower case"
+    //% subcategory="String"
+    //% weight=89
+    export function stringToLower(str: string): string {
+        return str.toLowerCase();
+    }
+
+    /**
+     * Convert a number to a string
+     */
+    //% block="number $num to string"
+    //% subcategory="String"
+    //% weight=88
+    export function numToString(num: number): string {
+        return num.toString();
+    }
+
+    /**
+     * Reverse a string
+     */
+    //% block="reverse string $str"
+    //% subcategory="String"
+    //% weight=87
+    export function reverseString(str: string): string {
+        let result = ""
+        for (let i = str.length - 1; i >= 0; i--) {
+            result += str.charAt(i)
+        }
+        return result
+    }
+
+    /**
+     * Repeat a string N times
+     */
+    //% block="repeat string $str $times times"
+    //% subcategory="String"
+    //% times.min=1 times.defl=2
+    //% weight=86
+    export function repeatString(str: string, times: number): string {
+        let result = ""
+        for (let i = 0; i < times; i++) {
+            result += str
+        }
+        return result
+    }
+
+    /**
+     * Check if string contains a substring
+     */
+    //% block="string $str contains $sub"
+    //% subcategory="String"
+    //% weight=85
+    export function stringContains(str: string, sub: string): boolean {
+        return str.indexOf(sub) >= 0
+    }
+
+    /**
+     * Replace all occurrences of a substring
+     */
+    //% block="in $str replace $find with $replaceWith"
+    //% subcategory="String"
+    //% inlineInputMode=inline
+    //% weight=84
+    export function stringReplace(str: string, find: string, replaceWith: string): string {
+        let result = ""
+        let i = 0
+        while (i < str.length) {
+            let match = true
+            for (let j = 0; j < find.length; j++) {
+                if (i + j >= str.length || str.charAt(i + j) !== find.charAt(j)) {
+                    match = false
+                    break
+                }
+            }
+            if (match && find.length > 0) {
+                result += replaceWith
+                i += find.length
+            } else {
+                result += str.charAt(i)
+                i++
+            }
+        }
+        return result
+    }
+
+    /**
+     * Pad a string on the left to reach desired length
+     */
+    //% block="pad $str to length $len with $padChar"
+    //% subcategory="String"
+    //% len.defl=5 padChar.defl="0"
+    //% weight=83
+    export function padLeft(str: string, len: number, padChar: string): string {
+        let result = str
+        while (result.length < len) {
+            result = padChar + result
+        }
+        return result
+    }
+
+    /**
+     * Count occurrences of a character in a string
+     */
+    //% block="count $char in $str"
+    //% subcategory="String"
+    //% weight=82
+    export function countChar(char: string, str: string): number {
+        let count = 0
+        for (let i = 0; i < str.length; i++) {
+            if (str.charAt(i) === char) count++
+        }
+        return count
     }
 
 }
